fc6b184d1a65c51ffaba8c66f0379f32e747c96e
chenerzhang <384076958@qq.com>
2015-06-15 11:15:16 +0800

    初赛完成版

--- end/fn3.php
+++ end/fn3.php
@@ -1,5 +1,6 @@
 <?php
-	if (isset($_POST['flag']) && $_POST['flag'] == '0') {
+	$flag = isset($_POST['flag']) && $_POST['flag'] == '0';
+	if ($flag) {
 		@$fp = fopen("wendu.txt", 'r'); 		//收到flag=0则以只读模式读取指令
 	} else {
 		@$fp = fopen("wendu.txt", 'r+'); 	//反之以读写模式读取指令
@@ -15,7 +16,7 @@
 		}
 	}
 	//在读写模式下清空文件内容
-	if (flock($fp, LOCK_EX)) {				//加写锁
+	if (!$flag && flock($fp, LOCK_EX)) {				//加写锁
 		ftruncate($fp, 0); 					// 将文件截断到给定的长度
 		rewind($fp); 						// 倒回文件指针的位置
 		fwrite($fp, '');						//  将文件内容清空，避免重复读
